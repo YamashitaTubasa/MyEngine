9897a13e325f16469bb1a73d27ad3d608d55573f
yamashitatubasa <k021g1247@g.neec.ac.jp>
2022-04-27 09:55:21 +0900

    02_03

--- main.cpp
+++ main.cpp
@@ -6,16 +6,13 @@
 #include <string>
 #include <DirectXMath.h>
 using namespace DirectX;
-
 #include <d3dcompiler.h>
+#define DIRECTINPUT_VERSION     0x0800   // DirectInputのバージョン指定
+#include <dinput.h>
 
 #pragma comment(lib, "d3d12.lib")
 #pragma comment(lib, "dxgi.lib")
 #pragma comment(lib, "d3dcompiler.lib")
-
-#define DIRECTINPUT_VERSION     0x0800   // DirectInputのバージョン指定
-#include <dinput.h>
-
 #pragma comment(lib, "dinput8.lib")
 #pragma comment(lib, "dxguid.lib")
 
