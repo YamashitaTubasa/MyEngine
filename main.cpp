23c845c33b349b8449322dd1a479159c2e72ca25
yamashitatubasa <k021g1247@g.neec.ac.jp>
2022-10-04 10:16:56 +0900

    10/4

--- main.cpp
+++ main.cpp
@@ -19,6 +19,32 @@ using namespace DirectX;
 
 float angle = 0.0f; // カメラの回転角
 
+// 定数バッファ用データ構造体（マテリアル）
+struct ConstBufferDataMaterial {
+	XMFLOAT4 color; // 色 (RGBA)
+};
+// 定数バッファ用データ構造体（3D変換行列）
+struct ConstBufferDataTransform {
+	XMMATRIX mat; // 3D変換行列
+};
+
+// 3Dオブジェクト型
+struct Object3d
+{
+	// 定数バッファ（行列用）
+	ID3D12Resource* constBuffTransform;
+	// 定数バッファマップ（行列用）
+	ConstBufferDataTransform* constMapTransform;
+	// アフィン変換情報
+	XMFLOAT3 scale = { 1,1,1 };
+	XMFLOAT3 rotation = { 0,0,0 };
+	XMFLOAT3 position = { 0,0,0 };
+	// ワールド変換行列
+	XMMATRIX matWorld;
+	// 親オブジェクトへのポインタ
+	Object3d* parent = nullptr;
+};
+
 // ウィンドウプロシージャ
 LRESULT WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) {
 	// メッセージに応じてゲーム固有の処理を行う
