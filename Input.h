417a3eafdf9c35e50539e4d83fcf075542581db0
yamashitatubasa <k021g1247@g.neec.ac.jp>
2022-10-11 11:45:57 +0900

    02_02.Inputのクラス化_更新編

--- Input.h
+++ Input.h
@@ -1,13 +1,23 @@
 #pragma once
 #include <windows.h>
+#include <wrl.h>
+#define DIRECTINPUT_VERSION 0x0800 // DirectInputのバージョン指定
+#include <dinput.h>
 
 // 入力
 class Input
 {
+public:
+	// namespace
+	template <class T> using ComPtr = Microsoft::WRL::ComPtr<T>;
+
 public: // メンバ関数
 	// 初期化
 	void Initialize(HINSTANCE hInstance, HWND hwnd);
 	// 更新
 	void Update();
+private: // メンバ変数
+	// キーボードのデバイス
+	ComPtr<IDirectInputDevice8> keyboard;
 };
 
