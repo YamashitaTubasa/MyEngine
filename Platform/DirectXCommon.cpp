be658da588c43820a8cb78f51a821db2b69b2fab
yamashitatubasa <k021g1247@g.neec.ac.jp>
2022-12-07 11:51:36 +0900

    FPSsleepの精度を上げた

--- Platform/DirectXCommon.cpp
+++ Platform/DirectXCommon.cpp
@@ -412,7 +412,7 @@ void DirectXCommon::UpdateFixFPS(){
 		std::chrono::duration_cast<std::chrono::microseconds>(now - reference_);
 
 	// 1/60秒(よりわずかに短い時間)経っていない場合
-	if (elapsed < kMinTime) {
+	if (elapsed < kMinCheckTime) {
 		// 1/60秒経過するまで微小名スリープを繰り返す
 		while (std::chrono::steady_clock::now() - reference_ < kMinTime) {
 			// 1マイクロ秒スリープ
